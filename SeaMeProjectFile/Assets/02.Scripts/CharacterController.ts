f2e2b7e4e9df4663cbb59bf5e14a1f3dd6c8fc2f
KimJaeSeop <greenseop@gmail.com>
2023-01-03 23:22:53 +0900

    플레이어 시점 세팅

--- SeaMeProjectFile/Assets/02.Scripts/CharacterController.ts
+++ SeaMeProjectFile/Assets/02.Scripts/CharacterController.ts
@@ -1,14 +1,21 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 import { SpawnInfo, ZepetoPlayers, LocalPlayer, ZepetoCharacter } from 
 'ZEPETO.Character.Controller'
+import { GameObject,Object } from 'UnityEngine';
  
 export default class CharacterController extends ZepetoScriptBehaviour {
-   Start() {        
+ 
+ 
+    @SerializeField()
+    private Test : GameObject;
+ 
+    Start() {        
        ZepetoPlayers.instance.CreatePlayerWithZepetoId("", "[ZEPETO_ID]", new SpawnInfo(), 
 true);
  
        ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
            let _player : LocalPlayer = ZepetoPlayers.instance.LocalPlayer;
+
        });
    }
 }
