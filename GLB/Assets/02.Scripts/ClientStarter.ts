8e548cb1e9770613d458cca15b61221f4317b9e9
KimJaeSeop <greenseop@gmail.com>
2023-01-15 00:06:06 +0900

    멀티 플레이 작업중

---
+++ GLB/Assets/02.Scripts/ClientStarter.ts
@@ -0,0 +1,69 @@
+import { Room, RoomData } from "ZEPETO.Multiplay";
+import { Player, State } from "ZEPETO.Multiplay.Schema";
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script';
+import { ZepetoWorldMultiplay } from 'ZEPETO.World';
+import { CharacterState, SpawnInfo, ZepetoPlayer, ZepetoPlayers } from "ZEPETO.Character.Controller"
+import * as UnityEngine from "UnityEngine";
+
+
+export default class ClientStarter extends ZepetoScriptBehaviour {
+
+    public multiPlay: ZepetoWorldMultiplay;
+    private room: Room;
+
+    private currentPlayers: Map<string, Player> = new Map<string, Player>();
+    Start() {
+        this.multiPlay.RoomCreated += (room: Room) => {
+            this.room = room;
+        }
+
+        this.multiPlay.RoomJoined += (room: Room) => {
+            room.OnStateChange += this.OnStateChange;
+        }
+    }
+
+    private OnStateChange(state: State, isFirst: boolean) {
+
+        if (isFirst) {
+            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(()=> {
+                const myPlayer = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
+                myPlayer.character.OnChangedState.AddListener((cur,pev) => {
+                    this.SendState(cur);
+                });
+            });
+        }
+
+        let join = new Map<string, Player>();
+        state.players.ForEach((sessionId: string, player: Player) => {
+            if (!this.currentPlayers.has(sessionId)) {
+                join.set(sessionId, player);
+            }
+
+        });
+        join.forEach((player: Player, sessionId: string) => this.OnJoinPlayer(sessionId, player));
+    }
+
+    private SendState(state: CharacterState){
+        const data = new RoomData();
+        data.Add("state",state);
+        this.room.Send("onChangedState",data.GetObject());
+    }
+
+    private OnJoinPlayer(sessionId: string, player: Player) {
+        console.log(`[OnJoinPlayer] player - sessionId : ${sessionId}`)
+        this.currentPlayers.set(sessionId, player);
+
+        const spawnInfo = new SpawnInfo();
+        const position = new UnityEngine.Vector3(0, 0, 0);
+        const rotation = new UnityEngine.Vector3(0, 0, 0);
+
+        spawnInfo.position = position;
+        spawnInfo.rotation = UnityEngine.Quaternion.Euler(rotation);
+
+        const isLocal = this.room.SessionId === player.sessionId;
+        ZepetoPlayers.instance.CreatePlayerWithUserId(sessionId, player.zepetoUserId, spawnInfo, isLocal);
+
+
+    }
+
+}
--- GLB/Assets/MultiSetting/Server.multiplay/index.ts
+++ GLB/Assets/MultiSetting/Server.multiplay/index.ts
@@ -1,13 +1,40 @@
 import { Sandbox, SandboxOptions, SandboxPlayer } from "ZEPETO.Multiplay";
+import { DataStorage } from "ZEPETO.Multiplay.DataStorage";
+import { Player } from "ZEPETO.Multiplay.Schema";
 
 export default class extends Sandbox {
 
     onCreate(options: SandboxOptions) {
-        
+        this.onMessage("onChangeState",(client,message)=>{
+            const Player = this.state.players.get(client.sessionId);
+            Player.state = message.state;
+        })
     }
 
-    onJoin(client: SandboxPlayer) {
+    async onJoin(client: SandboxPlayer) {
+        console.log(`OnJoin : ${client.sessionId} , HashCode : ${client.hashCode} , userId: ${client.userId}`)
 
+        const player = new Player();
+        player.sessionId = client.sessionId;
+
+      if (client.hashCode) {
+        player.zepetoHash = client.hashCode;
+      }
+      if(client.userId){
+        player.zepetoUserId = client.userId;
+      }
+      const storage : DataStorage = client.loadDataStorage();
+
+      let visit_count = await storage.get("VisitCount") as number;
+      if (visit_count ==null) {
+        visit_count =0;
+      }
+
+      console.log(`[onJoin] ${client.sessionId}'s visiting count : ${visit_count}`);
+
+      await storage.set(`VisitCount`,++visit_count);
+      this.state.players.set(client.sessionId , player);
+ 
     }
 
     onLeave(client: SandboxPlayer, consented?: boolean) {
