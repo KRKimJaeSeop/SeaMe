491d0f4565ed16e4ba81ab57a3cdb5171cfecd22
KimJaeSeop <greenseop@gmail.com>
2023-01-31 01:06:40 +0900

    제발요

--- GLB/Assets/02.Scripts/ClientStarter.ts
+++ GLB/Assets/02.Scripts/ClientStarter.ts
@@ -4,14 +4,28 @@ import { ZepetoScriptBehaviour } from 'ZEPETO.Script';
 import { ZepetoWorldMultiplay } from 'ZEPETO.World';
 import { CharacterState, SpawnInfo, ZepetoPlayer, ZepetoPlayers } from "ZEPETO.Character.Controller"
 import * as UnityEngine from "UnityEngine";
+import { Vector3, GameObject, Transform, WaitForSeconds } from 'UnityEngine';
 
 
 export default class ClientStarter extends ZepetoScriptBehaviour {
 
     public multiPlay: ZepetoWorldMultiplay;
     private room: Room;
+    private customCharacter : UnityEngine.GameObject
+
+    @SerializeField()
+    private customCharacter: UnityEngine.GameObject
+
+    // @SerializeField()
+    // private testParent: UnityEngine.GameObject
 
     private currentPlayers: Map<string, Player> = new Map<string, Player>();
+<<<<<<< HEAD
+=======
+    
+>>>>>>> 88c341d77f6cde41797ab71b40fb4f3fad8697ec
+
+
     Start() {
         this.multiPlay.RoomCreated += (room: Room) => {
             this.room = room;
@@ -19,15 +33,21 @@ export default class ClientStarter extends ZepetoScriptBehaviour {
 
         this.multiPlay.RoomJoined += (room: Room) => {
             room.OnStateChange += this.OnStateChange;
+<<<<<<< HEAD
+
+=======
+            
+>>>>>>> 88c341d77f6cde41797ab71b40fb4f3fad8697ec
         }
+        this.StartCoroutine(this.TestRoutine());
     }
 
     private OnStateChange(state: State, isFirst: boolean) {
 
         if (isFirst) {
-            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(()=> {
+            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
                 const myPlayer = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
-                myPlayer.character.OnChangedState.AddListener((cur,pev) => {
+                myPlayer.character.OnChangedState.AddListener((cur, pev) => {
                     this.SendState(cur);
                 });
             });
@@ -41,12 +61,13 @@ export default class ClientStarter extends ZepetoScriptBehaviour {
 
         });
         join.forEach((player: Player, sessionId: string) => this.OnJoinPlayer(sessionId, player));
+
     }
 
-    private SendState(state: CharacterState){
+    private SendState(state: CharacterState) {
         const data = new RoomData();
-        data.Add("state",state);
-        this.room.Send("onChangedState",data.GetObject());
+        data.Add("state", state);
+        this.room.Send("onChangedState", data.GetObject());
     }
 
     private OnJoinPlayer(sessionId: string, player: Player) {
@@ -57,13 +78,44 @@ export default class ClientStarter extends ZepetoScriptBehaviour {
         const position = new UnityEngine.Vector3(0, 0, 0);
         const rotation = new UnityEngine.Vector3(0, 0, 0);
 
+
+
         spawnInfo.position = position;
         spawnInfo.rotation = UnityEngine.Quaternion.Euler(rotation);
 
         const isLocal = this.room.SessionId === player.sessionId;
         ZepetoPlayers.instance.CreatePlayerWithUserId(sessionId, player.zepetoUserId, spawnInfo, isLocal);
+<<<<<<< HEAD
+
+        //UnityEngine.GameObject.Instantiate(this.customCharacter);
 
 
+
+=======
+      
+>>>>>>> 88c341d77f6cde41797ab71b40fb4f3fad8697ec
     }
 
+    *TestRoutine() {
+
+        yield new WaitForSeconds(3);
+
+        // 로컬 캐릭터
+        let _character = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
+
+        // 본 위치
+        let tempTransform = _character.character.ZepetoAnimator.GetBoneTransform(UnityEngine.HumanBodyBones.Head);
+        let tempVector = new Vector3(tempTransform.position.x, tempTransform.position.y, tempTransform.position.z);
+
+        // Instantiate한 프리팹
+        let _gameObject = UnityEngine.GameObject.Instantiate(this.customCharacter,tempTransform)as GameObject ;
+
+        _gameObject.transform.SetParent(tempTransform);
+        //    tempGO.gameObject.transform.position = tempTransform);
+
+
+        //console.log(`[OnJoinPlayer] parentPosition :: ${this.testParent.transform}`)
+        //console.log(`[OnJoinPlayer] GO Name :: ${tempGO}`)
+
+    }
 }
--- GLB/Assets/MultiSetting/Server.multiplay/index.ts
+++ GLB/Assets/MultiSetting/Server.multiplay/index.ts
@@ -1,43 +1,51 @@
+import { getAllJSDocTags } from "typescript";
 import { Sandbox, SandboxOptions, SandboxPlayer } from "ZEPETO.Multiplay";
 import { DataStorage } from "ZEPETO.Multiplay.DataStorage";
 import { Player } from "ZEPETO.Multiplay.Schema";
 
 export default class extends Sandbox {
 
-    onCreate(options: SandboxOptions) {
-        this.onMessage("onChangeState",(client,message)=>{
-            const Player = this.state.players.get(client.sessionId);
-            Player.state = message.state;
-        })
+  // Room이 생성될 때 1회 호출한다.
+  onCreate(options: SandboxOptions) {
+    this.onMessage("onChangeState", (client, message) => {
+      const Player = this.state.players.get(client.sessionId);
+      Player.state = message.state;
+    })
+  }
+
+  // Client가 Room에 입장할 때 호출된다. Client의 ID 및 캐릭터 정보는 SandboxPlayer 객체에 포함되어있다.
+  async onJoin(client: SandboxPlayer) {
+    console.log(`OnJoin : ${client.sessionId} , HashCode : ${client.hashCode} , userId: ${client.userId}`)
+
+    const player = new Player();
+    player.sessionId = client.sessionId;
+
+    if (client.hashCode) {
+      player.zepetoHash = client.hashCode;
+    }
+    if (client.userId) {
+      player.zepetoUserId = client.userId;
+    }
+    // Client의 데이터
+    const storage: DataStorage = client.loadDataStorage();
+
+    let visit_count = await storage.get("VisitCount") as number;
+    if (visit_count == null) {
+      visit_count = 0;
     }
 
-    async onJoin(client: SandboxPlayer) {
-        console.log(`OnJoin : ${client.sessionId} , HashCode : ${client.hashCode} , userId: ${client.userId}`)
+    console.log(`[onJoin] ${client.sessionId}'s visiting count : ${visit_count}`);
+    await storage.set(`VisitCount`, ++visit_count);
+    this.state.players.set(client.sessionId, player);
 
-        const player = new Player();
-        player.sessionId = client.sessionId;
+  }
+  // Client가 Room에서 퇴장할 때 호출된다. 
+  //연결 해제를 요청한 경우  Consented가 true.
+  onLeave(client: SandboxPlayer, consented?: boolean) {
 
-      if (client.hashCode) {
-        player.zepetoHash = client.hashCode;
-      }
-      if(client.userId){
-        player.zepetoUserId = client.userId;
-      }
-      const storage : DataStorage = client.loadDataStorage();
+  }
+  // SandboxOptions 에서 설정된 tickInterval마다 호출(update문임). 여기서 시간 체크
+      OnTick(deltaTIme: number) {
 
-      let visit_count = await storage.get("VisitCount") as number;
-      if (visit_count ==null) {
-        visit_count =0;
-      }
-
-      console.log(`[onJoin] ${client.sessionId}'s visiting count : ${visit_count}`);
-
-      await storage.set(`VisitCount`,++visit_count);
-      this.state.players.set(client.sessionId , player);
- 
-    }
-
-    onLeave(client: SandboxPlayer, consented?: boolean) {
-        
-    }
+  }
 }
